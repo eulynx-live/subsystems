20a60c4e81eb8709c1336ddb669259ffe7813381
LeandroParisi <lepcbelisario@gmail.com>
2023-04-13 09:45:17 +0200

    refac: switch statement syntax

--- src/Point/Services/Extensions/ReportedPointPositionProtoConversion.cs
+++ src/Point/Services/Extensions/ReportedPointPositionProtoConversion.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using EulynxLive.Messages.Baseline4R1;
 using EulynxLive.Point.Proto;
 using ReportedPointPosition = EulynxLive.Messages.Baseline4R1.PointPointPositionMessageReportedPointPosition;
 
@@ -10,38 +6,22 @@ namespace Point.Services.Extensions
 {
     public static class ReportedPointPositionProtoConversion
     {
-        public static PointPosition ConvertToProtoMessage(this ReportedPointPosition reportedPointPosition)
+        public static PointPosition ConvertToProtoMessage(this ReportedPointPosition reportedPointPosition) => reportedPointPosition switch
         {
-            switch (reportedPointPosition)
-            {
-                case ReportedPointPosition.PointIsInARightHandPositionDefinedEndPosition:
-                    return PointPosition.Right;
-                case ReportedPointPosition.PointIsInALeftHandPositionDefinedEndPosition:
-                    return PointPosition.Left;
-                case ReportedPointPosition.PointIsInNoEndPosition:
-                    return PointPosition.NoEndPosition;
-                case ReportedPointPosition.PointIsTrailed:
-                    return PointPosition.Trailed;
-                default:
-                    throw new InvalidCastException($"Unable to convert reported point position {reportedPointPosition} to proto enum");
-            }
-        }
+            ReportedPointPosition.PointIsInARightHandPositionDefinedEndPosition => PointPosition.Right,
+            ReportedPointPosition.PointIsInALeftHandPositionDefinedEndPosition => PointPosition.Left,
+            ReportedPointPosition.PointIsInNoEndPosition => PointPosition.NoEndPosition,
+            ReportedPointPosition.PointIsTrailed => PointPosition.Trailed,
+            _ => throw new InvalidCastException($"Unable to convert reported point position {reportedPointPosition} to proto enum")
+        };
 
-        public static ReportedPointPosition ConvertToReportedPointPosition(this PointPosition pointPositionMessage)
+        public static ReportedPointPosition ConvertToReportedPointPosition(this PointPosition pointPositionMessage) => pointPositionMessage switch
         {
-            switch (pointPositionMessage)
-            {
-                case PointPosition.Right:
-                    return ReportedPointPosition.PointIsInARightHandPositionDefinedEndPosition;
-                case PointPosition.Left:
-                    return ReportedPointPosition.PointIsInALeftHandPositionDefinedEndPosition;
-                case PointPosition.NoEndPosition:
-                    return ReportedPointPosition.PointIsInNoEndPosition;
-                case PointPosition.Trailed:
-                    return ReportedPointPosition.PointIsTrailed;
-                default:
-                    throw new InvalidCastException($"Unable to convert point position message {pointPositionMessage} to reported point position.");
-            }
-        }
+            PointPosition.Right => ReportedPointPosition.PointIsInARightHandPositionDefinedEndPosition,
+            PointPosition.Left => ReportedPointPosition.PointIsInALeftHandPositionDefinedEndPosition,
+            PointPosition.NoEndPosition => ReportedPointPosition.PointIsInNoEndPosition,
+            PointPosition.Trailed => ReportedPointPosition.PointIsTrailed,
+            _ => throw new InvalidCastException($"Unable to convert point position message {pointPositionMessage} to reported point position.")
+        };
     }
 }
