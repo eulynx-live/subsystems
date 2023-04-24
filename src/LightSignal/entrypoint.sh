723133daba65bdbbf6b40e1a635dde6e893757b2
linebacker12 <linebacker12@users.noreply.github.com>
2023-04-24 20:19:25 +0200

    entrypoint.sh commented through

--- src/LightSignal/entrypoint.sh
+++ src/LightSignal/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-
+# Check if environment variable LOCAL_IDS was set and throw an error if missing or create an array. LOCAL_IDS may be empty, it just has to exist.
 if [[ -z "${LOCAL_IDS}" ]]; then
   echo "LOCAL_IDS was not set. All light signals found in Interlocking.exml will be simulated!"
 else
+   # Internal Field Separator was used to split LOCAL_IDS the single entries into an array
    IFS=',' read -r -a LOCAL_IDS <<< "$LOCAL_IDS"
 fi
 
+# Check other environment variables and set a default value if they are not set
 if [[ -z "${REMOTE_ID}" ]]; then
   REMOTE_ID="INTERLOCKING"
   echo "REMOTE_ID was not set, default to $REMOTE_ID"
@@ -35,6 +37,7 @@ do
    read -r f1 <&3 || break
    read -r f2 <&4 || break
    name=$( xq -x "//generic:ownsSignal/rsmCommon:id[.=\"$f1\"]/following-sibling::rsmCommon:name" /config/Interlocking.exml )
+   # Compare all entries of the array LOCAL_IDS with the names of the light signals found in Interlocking.xml. If LOCAL_IDS is empty, use all signals present in the Interlocking.exml.
    if (printf '%s\0' "${LOCAL_IDS[@]}" | grep -Fxzq -- "$name") || [[ -z "${LOCAL_IDS}" ]] ;
    then 
       echo -n "LightSignals__${COUNTER}__id=$name "
